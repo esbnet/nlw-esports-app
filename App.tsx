7bf3ebc94bdef4b3856eaeb99a6237d0f8f75931
Edmilson Soares <esbnet@gmail.com>
2022-09-11 23:57:47 -0300

    feat: :sparkles: start project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.title}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Word!</Text>
+      <Text style={styles.title}>
+        Hello Word!
+      </Text>
+      <Button title='Clique aqui'/>
+      <Button title='Clique aqui 2'/>
+      <Button title='Clique aqui 3'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  title: {
+    color: '#fff',
+    fontSize: 22
+  }
 });
